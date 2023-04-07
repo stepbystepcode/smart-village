d4b59f46b184d13b1a6b41515b30da1c6e414bd6
LQ <2996918437@qq.com>
2023-04-07 19:05:14 +0800

    def

--- src/main/kotlin/webplus/smartvillage/SmartvillageApplication.kt
+++ src/main/kotlin/webplus/smartvillage/SmartvillageApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 
-
+//def
 @SpringBootApplication
 class SmartVillageApplication
 
