e65f8789ee52f59e0a1c9473c7a58500565c0981
Li Jing <abc1870819884@gmail.com>
2023-04-11 21:23:40 +0800

    divide url request params

--- src/main/kotlin/webplus/smartvillage/DataController.kt
+++ src/main/kotlin/webplus/smartvillage/DataController.kt
@@ -3,17 +3,16 @@ package webplus.smartvillage
 import org.bson.Document
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.web.bind.annotation.*
-
-
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/api/data/{collection}")
 class DataController(val mongoTemplate: MongoTemplate) {
-    @GetMapping("/api/data/{collection}")
+    @GetMapping("/")
     fun getData(@PathVariable collection: String): List<Document>{
         val query = Document()
         return mongoTemplate.getCollection(collection).find(query).toList()
     }
-    @GetMapping("/api/data/{collection}/{id}")
+    @GetMapping("/{id}")
     fun getOne(@PathVariable collection: String,@PathVariable id: Number): List<Document>{
         val query = Document("id",id)
         return mongoTemplate.getCollection(collection).find(query).toList()
