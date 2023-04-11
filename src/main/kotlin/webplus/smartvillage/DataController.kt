4fa701679b8fc750bc5b2dc8283d7dac901eceaa
Li Jing <3202146864@qq.com>
2023-04-11 14:11:29 +0800

    findone

--- src/main/kotlin/webplus/smartvillage/DataController.kt
+++ src/main/kotlin/webplus/smartvillage/DataController.kt
@@ -2,17 +2,20 @@ package webplus.smartvillage
 
 import org.bson.Document
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
+@CrossOrigin("*")
 class DataController(val mongoTemplate: MongoTemplate) {
-    @CrossOrigin("*")
     @GetMapping("/api/data/{collection}")
     fun getData(@PathVariable collection: String): List<Document>{
         val query = Document()
+        return mongoTemplate.getCollection(collection).find(query).toList()
+    }
+    @GetMapping("/api/data/{collection}/{id}")
+    fun getOne(@PathVariable collection: String,@PathVariable id: Number): List<Document>{
+        val query = Document("id",id)
         return mongoTemplate.getCollection(collection).find(query).toList()
     }
 }
